6310372d1bff7f7eab9cac5db30ca119f204319d
AndrewLi <13058100289@163.com>
2019-07-21 17:56:16 +0800

    add page header component and add CSS

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="todo-container">
+    <h1>todos</h1>
+    <div class="todo-wrap">
+      <todo-header :addTodo="addTodo" />
+    </div>
   </div>
 </template>
 
 <script>
+import TodoHeader from "./components/TodoHeader";
+
 export default {
-  name: 'App'
-}
+  data() {
+    return {
+      todos: [],
+      visibility: 'all'
+    };
+  },
+  components: {
+    TodoHeader
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+body {
+  font: 14px "Helvetica Neue", Helvetica, Arial, sans-serif;
+  line-height: 1.4em;
+  background: #f5f5f5;
+  color: #4d4d4d;
+  min-width: 230px;
+  max-width: 550px;
+  margin: 0 auto;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-weight: 300;
+}
+
+.todo-container h1 {
+  width: 100%;
+  font-size: 100px;
+  font-weight: 100;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: rgba(175, 47, 47, 0.15);
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+}
+
+.todo-wrap {
+  background: #fff;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
 }
 </style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="todo-header">
+    <input v-model="title" @keyup.enter="addItem" type="text" class="new-todo" placeholder="What needs to be done" />
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+.new-todo {
+  padding: 16px 16px 16px 60px;
+  border: none;
+  background: rgba(0, 0, 0, 0.003);
+  box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+
+  font-size: 24px;
+  font-family: inherit;
+  font-weight: inherit;
+  line-height: 1.4em;
+}
+
+.todo-header .edit {
+  position: relative;
+  margin: 0;
+  width: 100%;
+  font-size: 24px;
+  font-family: inherit;
+  font-weight: inherit;
+  line-height: 1.4em;
+  border: 0;
+  color: inherit;
+  padding: 6px;
+  border: 1px solid #999;
+  box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+input {
+  width: 86%;
+  text-rendering: auto;
+  color: initial;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-transform: none;
+  text-indent: 0px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  margin: 0em;
+  font: 400 11px system-ui;
+
+  outline: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+input::-webkit-input-placeholder {
+  padding-left: 30px;
+  opacity: 0.5;
+}
+</style>
